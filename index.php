9f3c9d1cfd0ff1c617f3da1544a3a4d15a1d61a0
Lilia <geertszelia@gmail.com>
2017-01-24 16:57:24 +0100

    Ajout du top 10 des films

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
-	$string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
-	$dico = explode("\n", $string);
+	$stringDico = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
+	$dico = explode("\n", $stringDico);
+	
+	$stringFilm = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
+	$brut = json_decode($stringFilm, true);
+	$top = $brut["feed"]["entry"]; # liste de films
 ?>
 
 <!DOCTYPE html>
@@ -48,6 +52,26 @@
 			endforeach;
 			echo $count;
 		?> mots finissent par la lettre « q »</li>
+	</ol>
+	
+	<h2>Film</h2>
+	<ol>
+		<li>Top 10 des films:<ol>
+			<?php for($film = 0; $film < 10; $film++) { ?>
+				<li><?php echo $top[$film]['im:name']['label']; ?></li>
+			<?php } ?>
+		</ol></li>
+		<li>Le film "Gravity" est classé
+			
+		</li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
 	</ol>
 </body>
 </html>
